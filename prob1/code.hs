d086ab83b3f50b761437b80bdec815eaafd1cd12
Ogg <ogg.developer@gmail.com>
2022-12-01 07:14:55 +0100

    Refactored prob1

--- prob1/code.hs
+++ prob1/code.hs
@@ -2,27 +2,14 @@ import           Data.List.Split
 import           Data.List
 import           Data.Ord
 
-part1 :: IO ()
-part1 = do
-  str <- readFile "prob1/input.txt"
-  let elfSplit  = splitOn "\n\n" str
-  let lineSplit = map lines elfSplit
-  let elfNums   = map (\l -> map read l :: [Int]) lineSplit
-  let elfTotals = map sum elfNums
-  let maxTotal  = maximum elfTotals
-  print maxTotal
-
-part2 :: IO ()
-part2 = do
-  str <- readFile "prob1/input.txt"
-  let elfSplit        = splitOn "\n\n" str
-  let lineSplit       = map lines elfSplit
-  let elfNums         = map (\l -> map read l :: [Int]) lineSplit
-  let elfTotals       = map sum elfNums
-  let sortedElfTotals = sortOn Down elfTotals
-  let maxTotal3       = sum (take 3 sortedElfTotals)
-  print maxTotal3
-
+getTotals :: String -> [Int]
+getTotals = map (sum . map read . lines) . splitOn "\n\n"
 
 main :: IO ()
-main = part2
+main = do
+  elfTotals <- getTotals <$> readFile "prob1/input.txt"
+  let sortedElfTotals = sortOn Down elfTotals
+  print $ head sortedElfTotals
+  print $ sum $ take 3 sortedElfTotals
+
+
