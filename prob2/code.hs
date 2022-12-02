958c9711e9a93a95e293d2eae08bacfe546b698c
Ogg <ogg.developer@gmail.com>
2022-12-02 06:58:52 +0100

    changed to 0 indexed picks

--- prob2/code.hs
+++ prob2/code.hs
@@ -7,23 +7,21 @@ outcomeScore :: (Int, Int) -> Int
 outcomeScore (o, m) = [0, 3, 6] !! ((1 + m - o) `mod` 3)
 
 scoreFor :: (Int, Int) -> Int
-scoreFor (o, m) = outcomeScore (o, m) + m
+scoreFor (o, m) = (m + 1) + outcomeScore (o, m)
 
 toNums :: String -> (Int, Int)
 toNums s =
-  ( fromJust (elemIndex (head s) "ABC") + 1
-  , fromJust (elemIndex (last s) "XYZ") + 1
-  )
+  (fromJust (elemIndex (head s) "ABC"), fromJust (elemIndex (last s) "XYZ"))
   where w = words s
 
 fromOutcome :: (Int, Int) -> (Int, Int)
-fromOutcome (o, 1) = (o, (o - 2) `mod` 3 + 1)
-fromOutcome (o, 2) = (o, o)
-fromOutcome (o, 3) = (o, o `mod` 3 + 1)
+fromOutcome (o, 0) = (o, (o - 1) `mod` 3)
+fromOutcome (o, 1) = (o, o)
+fromOutcome (o, 2) = (o, (o + 1) `mod` 3)
 
 main :: IO ()
 main = do
   content <- readFile "prob2/input.txt"
-  let strat = lines content
-  print (sum (map (scoreFor . toNums) strat))
-  print (sum (map (scoreFor . fromOutcome . toNums) strat))
+  let strat = map toNums $ lines content
+  print $ sum $ map scoreFor strat
+  print $ sum $ map (scoreFor . fromOutcome) strat
