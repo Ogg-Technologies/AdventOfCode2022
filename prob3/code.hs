2b32fb0b9653d3bfb20e3664e73b9e9e47607572
Ogg <ogg.developer@gmail.com>
2022-12-03 07:02:16 +0100

    refactored prob3

--- prob3/code.hs
+++ prob3/code.hs
@@ -1,33 +1,28 @@
-import           Data.List.Split
 import           Data.List
-import           Data.Ord
 import           Data.Maybe
 
+prioritiesList = "-abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+prio :: Char -> Int
+prio c = fromJust (elemIndex c prioritiesList)
 
-prio = "-abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-
-tripples :: [String] -> [(String, String, String)]
+tripples :: [String] -> [[String]]
 tripples = tripples_ []
 
-tripples_
-  :: [(String, String, String)] -> [String] -> [(String, String, String)]
+tripples_ :: [[String]] -> [String] -> [[String]]
 tripples_ acc []               = acc
-tripples_ acc (x : y : z : xs) = tripples_ (acc ++ [(x, y, z)]) xs
+tripples_ acc (x : y : z : xs) = tripples_ (acc ++ [[x, y, z]]) xs
+
+splitMiddle :: String -> [String]
+splitMiddle s = [x, y] where (x, y) = splitAt (length s `div` 2) s
+
+intersectList :: [String] -> String
+intersectList (x : xs) = foldl intersect x xs
+
+getPrioSum :: [[String]] -> Int
+getPrioSum elems = sum $ map (prio . head . intersectList) elems
 
 main :: IO ()
 main = do
-  content <- readFile "prob3/input.txt"
-  let a = lines content
-  let b = tripples a
-  print b
-  let c = map (\(x, y, z) -> head $ intersect (x `intersect` y) z) b
-  let d = map (\x -> fromJust (elemIndex x prio)) c
-  print $ sum d
-
-  --content <- readFile "prob3/input.txt"
-  --let a = lines content
-  --let b = map (\l -> splitAt (length l `div` 2) l) a
-  --let c = map (uncurry intersect) b
-  --let d = map head c
-  --let e = map (\x -> fromJust (elemIndex x prio)) d
-  --print $ sum e
+  l <- lines <$> readFile "prob3/input.txt"
+  print $ getPrioSum $ map splitMiddle l
+  print $ getPrioSum $ tripples l
