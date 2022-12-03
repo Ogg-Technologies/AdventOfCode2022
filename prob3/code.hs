c53f9226d96c4817910ca68dd4a86f262a12b663
Ogg <ogg.developer@gmail.com>
2022-12-03 07:41:56 +0100

    moved to using builtin functions

--- prob3/code.hs
+++ prob3/code.hs
@@ -1,28 +1,18 @@
 import           Data.List
-import           Data.Maybe
+import           Data.List.Split
+import           Data.Char
 
-prioritiesList = "-abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 prio :: Char -> Int
-prio c = fromJust (elemIndex c prioritiesList)
-
-tripples :: [String] -> [[String]]
-tripples = tripples_ []
-
-tripples_ :: [[String]] -> [String] -> [[String]]
-tripples_ acc []               = acc
-tripples_ acc (x : y : z : xs) = tripples_ (acc ++ [[x, y, z]]) xs
+prio c = ord c + (if isUpper c then 27 - ord 'A' else 1 - ord 'a')
 
 splitMiddle :: String -> [String]
 splitMiddle s = [x, y] where (x, y) = splitAt (length s `div` 2) s
 
-intersectList :: [String] -> String
-intersectList (x : xs) = foldl intersect x xs
-
 getPrioSum :: [[String]] -> Int
-getPrioSum elems = sum $ map (prio . head . intersectList) elems
+getPrioSum elems = sum $ map (prio . head . foldr1 intersect) elems
 
 main :: IO ()
 main = do
   l <- lines <$> readFile "prob3/input.txt"
   print $ getPrioSum $ map splitMiddle l
-  print $ getPrioSum $ tripples l
+  print $ getPrioSum $ chunksOf 3 l
