dd2c356fa0164647601f38b5d35385ebbc885d6e
Ogg <ogg.developer@gmail.com>
2022-12-04 07:03:53 +0100

    changed to filter

--- prob4/code.hs
+++ prob4/code.hs
@@ -4,18 +4,14 @@ parseRow :: String -> (Int, Int, Int, Int)
 parseRow s = (l1, h1, l2, h2)
   where [l1, h1, l2, h2] = map read $ concatMap (splitOn "-") $ splitOn "," s
 
-withinCount :: (Int, Int, Int, Int) -> Int
-withinCount (l1, h1, l2, h2) | l1 <= l2 && h1 >= h2 = 1
-                             | l2 <= l1 && h2 >= h1 = 1
-                             | otherwise            = 0
+within :: (Int, Int, Int, Int) -> Bool
+within (l1, h1, l2, h2) = l1 <= l2 && h1 >= h2 || l2 <= l1 && h2 >= h1
 
-overlapCount :: (Int, Int, Int, Int) -> Int
-overlapCount (l1, h1, l2, h2) | l1 <= h2 && h1 >= l2 = 1
-                              | otherwise            = 0
-
+overlaps :: (Int, Int, Int, Int) -> Bool
+overlaps (l1, h1, l2, h2) = l1 <= h2 && h1 >= l2
 
 main :: IO ()
 main = do
   l <- map parseRow . lines <$> readFile "prob4/input.txt"
-  print $ sum $ map withinCount l
-  print $ sum $ map overlapCount l
+  print $ length $ filter within l
+  print $ length $ filter overlaps l
