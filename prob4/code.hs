ab6153669e9c65b17e4bb8ba7fea85f65c6d9098
Ogg <ogg.developer@gmail.com>
2022-12-04 06:38:46 +0100

    renamed variables

--- prob4/code.hs
+++ prob4/code.hs
@@ -3,21 +3,21 @@ import           Data.List.Split
 import           Data.Char
 
 parseRow :: String -> (Int, Int, Int, Int)
-parseRow s = (a, b, c, d)
-  where [a, b, c, d] = map read $ concatMap (splitOn "-") $ splitOn "," s
+parseRow s = (l1, h1, l2, h2)
+  where [l1, h1, l2, h2] = map read $ concatMap (splitOn "-") $ splitOn "," s
 
 withinCount :: (Int, Int, Int, Int) -> Int
-withinCount (a, b, c, d) | a <= c && b >= d = 1
-                         | c <= a && d >= b = 1
-                         | otherwise        = 0
+withinCount (l1, h1, l2, h2) | l1 <= l2 && h1 >= h2 = 1
+                             | l2 <= l1 && h2 >= h1 = 1
+                             | otherwise            = 0
 
 overlapCount :: (Int, Int, Int, Int) -> Int
-overlapCount (a, b, c, d) | a <= d && b >= c = 1
-                          | otherwise        = 0
+overlapCount (l1, h1, l2, h2) | l1 <= h2 && h1 >= l2 = 1
+                              | otherwise            = 0
 
 
 main :: IO ()
 main = do
-  l <- lines <$> readFile "prob4/input.txt"
-  print $ sum $ map (withinCount . parseRow) l
-  print $ sum $ map (overlapCount . parseRow) l
+  l <- map parseRow . lines <$> readFile "prob4/input.txt"
+  print $ sum $ map withinCount l
+  print $ sum $ map overlapCount l
