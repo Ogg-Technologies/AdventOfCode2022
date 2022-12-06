604594ca30aad3853e883d673deab0f898e08287
Ogg <ogg.developer@gmail.com>
2022-12-06 07:20:42 +0100

    replaced with standard function

--- prob6/code.hs
+++ prob6/code.hs
@@ -1,19 +1,16 @@
 import           Data.List.Split
 import           Data.List
-import           Data.Char
 import           Data.Maybe
 
-windowed :: Int -> Int -> String -> [String]
-windowed size step str
-  | length str <= size = [str]
-  | otherwise          = take size str : windowed size step (drop step str)
+firstUnique :: [String] -> Int
+firstUnique = fromJust . findIndex (\s -> nub s == s)
 
+findMarker :: Int -> String -> Int
+findMarker n s = firstUnique (divvy n 1 s) + n
 
 main :: IO ()
 main = do
   l <- readFile "prob6/input.txt"
-  let w1 = windowed 4 1 l
-  print $ fromJust (findIndex (\s -> nub s == s) w1) + length (head w1)
-  let w2 = windowed 14 1 l
-  print $ fromJust (findIndex (\s -> nub s == s) w2) + length (head w2)
+  print $ findMarker 4 l
+  print $ findMarker 14 l
 
