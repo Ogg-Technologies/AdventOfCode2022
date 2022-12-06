fd607544703112d1873a2bb2d30752bcf5cf75f2
Ogg <ogg.developer@gmail.com>
2022-12-06 06:24:28 +0100

    cleaned up prob6

--- prob6/code.hs
+++ prob6/code.hs
@@ -6,13 +6,14 @@ import           Data.Maybe
 windowed :: Int -> Int -> String -> [String]
 windowed size step str
   | length str <= size = [str]
-  | otherwise          = [take size str] ++ (windowed size step (drop step str))
+  | otherwise          = take size str : windowed size step (drop step str)
 
 
 main :: IO ()
 main = do
   l <- readFile "prob6/input.txt"
-  let a = windowed 14 1 l
-  let b = fromJust (findIndex (\s -> nub s == s) a) + 14
-  print $ b
+  let w1 = windowed 4 1 l
+  print $ fromJust (findIndex (\s -> nub s == s) w1) + length (head w1)
+  let w2 = windowed 14 1 l
+  print $ fromJust (findIndex (\s -> nub s == s) w2) + length (head w2)
 
