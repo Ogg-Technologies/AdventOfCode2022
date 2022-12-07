4891b1ef757dd05549f9611c38fd46c28c23e03d
Ogg <ogg.developer@gmail.com>
2022-12-07 12:38:42 +0100

    working solution for prob7

--- prob7/code.hs
+++ prob7/code.hs
@@ -2,14 +2,6 @@ import           Data.List.Split
 import           Data.List
 import           Data.Maybe
 
-
-{-
--- Filesystem is
--- a directory with name and other filesystems
--- or a file with a size and name
-data FS = Dir String [FS] | File Int String
-  deriving Show
-
 data Cmd = Cd String | Ls [String]
   deriving Show
 
@@ -17,51 +9,50 @@ toCmd :: [String] -> Cmd
 toCmd (c : rs) | c == "ls"        = Ls rs
                | take 2 c == "cd" = Cd (drop 3 c)
 
-
-rToFS :: String -> FS
-rToFS r | w1 == "dir" = Dir w2 []
-        | otherwise   = File (read w1) w2
-  where [w1, w2] = words r
-
-writeToPath :: FS -> [String] -> [String] -> FS
-writeToPath (Dir _ l) [p] rs = Dir $ take i l ++ [Dir p []] ++ drop (i + 1) l
- where
-  i      = findIndex (\(Dir n _) -> n == p) l
-  newDir = Dir p (map rToFS rs)
-
-
-traverse :: (FS, [String]) -> Cmd -> (FS, [String])
-traverse (fs, path) (Cd s ) = (fs, path ++ "/" ++ s)
-traverse (fs, path) (Ls rs) = (fs, path)
--}
-
-data Cmd = Cd String | Ls [String]
-  deriving Show
-
-toCmd :: [String] -> Cmd
-toCmd (c : rs) | c == "ls"        = Ls rs
-               | take 2 c == "cd" = Cd (drop 3 c)
-
-rsToFiles :: [String] -> [(Int, String)]
-rsToFiles rs = map (\r -> (read (head (words r)), last (words r))) fileStrings
+rsToFiles :: String -> [String] -> [(Int, String)]
+rsToFiles path rs = map
+  (\r -> (read (head (words r)), path ++ "/" ++ last (words r)))
+  fileStrings
   where fileStrings = filter (\r -> head (words r) /= "dir") rs
 
+newPath :: String -> String -> String
+newPath path ".." = take (last (elemIndices '/' path)) path
+newPath path s    = path ++ "/" ++ s
 
 -- trav files path cmd -> newFiles
 trav :: ([(Int, String)], String) -> [Cmd] -> [(Int, String)]
-trav (files, path) []               = files
-trav (files, path) ((Cd s ) : cmds) = trav (files, (path ++ "/" ++ s)) cmds
-trav (files, path) ((Ls rs) : cmds) = trav ((files ++ rsToFiles rs), path) cmds
+trav (files, path) []              = files
+trav (files, path) ((Cd s) : cmds) = trav (files, newPath path s) cmds
+trav (files, path) ((Ls rs) : cmds) =
+  trav (files ++ rsToFiles path rs, path) cmds
+
+getDirectories :: [(Int, String)] -> [String]
+getDirectories files = nub $ do
+  f <- fileNames
+  let slashes = elemIndices '/' f
+  let ds      = map (\i -> take i f ++ "/") slashes
+  ds
+  where fileNames = map snd files
+
+getDirSize :: String -> [(Int, String)] -> Int
+getDirSize dir files =
+  sum $ map fst $ filter (\(s, f) -> dir `isPrefixOf` f) files
 
 main :: IO ()
 main = do
-  l <- readFile "prob7/mini_input.txt"
-  let a = drop 1 $ splitOn "\n$ " l
-  let b = map lines a
-  let c = map toCmd b
-  print c
+  l <- readFile "prob7/input.txt"
+  let a        = drop 1 $ splitOn "\n$ " l
+  let b        = map lines a
+  let cmds     = map toCmd b
+  let allFiles = trav ([], "") cmds
+  let dirSizes =
+        map (\d -> (d, getDirSize d allFiles)) (getDirectories allFiles)
+  print $ sum $ map snd $ filter (\(d, s) -> s < 100000) dirSizes
 
 
-  print $ trav ([], "") [Cd "a"]
-
-  print $ trav ([], "") c
+  let usedSpace     = sum $ map fst allFiles
+  let capacity      = 70000000
+  let requiredSpace = 30000000
+  let toDelete      = usedSpace - (capacity - requiredSpace)
+  print toDelete
+  print $ minimum $ filter (> toDelete) $ map snd dirSizes
