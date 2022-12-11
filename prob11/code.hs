ff4ec8e46cc5a6fcde0c5f2fd3ddb43a6710e4bf
Ogg <ogg.developer@gmail.com>
2022-12-11 07:34:17 +0100

    finished prob11

--- prob11/code.hs
+++ prob11/code.hs
@@ -5,38 +5,41 @@ import           Data.Char
 import           Data.Ord
 
 
-parseStarting :: String -> [Integer]
+parseStarting :: String -> [Int]
 parseStarting s = map (read . filter isDigit) (drop 2 $ words (lines s !! 1))
 
-parseOp :: String -> Integer -> Integer
+parseOp :: String -> Int -> Int
 parseOp s old = old `op` other
  where
   ws    = drop 3 $ words (lines s !! 2)
   op    = if (ws !! 1) == "*" then (*) else (+)
   other = if (ws !! 2) == "old" then old else read (ws !! 2)
 
-parseTest :: String -> Integer -> Int
+parseTest :: String -> Int -> Int
 parseTest s n = if n `mod` divN == 0 then tCase else fCase
  where
   divN  = read $ last $ words (lines s !! 3)
   tCase = read $ last $ words (lines s !! 4)
   fCase = read $ last $ words (lines s !! 5)
 
+parseModVal :: String -> Int
+parseModVal s = read $ last $ words (lines s !! 3)
+
 (!!=) :: [a] -> (Int, a) -> [a]
 (!!=) xs (i, x) = take i xs ++ [x] ++ drop (i + 1) xs
 
 doActions
-  :: ([[Integer]], [Int], [Integer -> Integer], [Integer -> Int])
+  :: ([[Int]], [Int], [Int -> Int], [Int -> Int], Int)
   -> Int
-  -> ([[Integer]], [Int], [Integer -> Integer], [Integer -> Int])
-doActions (curr, totInspections, ops, tests) i
-  | null (curr !! i) = (curr, totInspections, ops, tests)
-  | otherwise        = doActions (newCurr, newTot, ops, tests) i
+  -> ([[Int]], [Int], [Int -> Int], [Int -> Int], Int)
+doActions (curr, totInspections, ops, tests, modVal) i
+  | null (curr !! i) = (curr, totInspections, ops, tests, modVal)
+  | otherwise        = doActions (newCurr, newTot, ops, tests, modVal) i
  where
   op         = ops !! i
   test       = tests !! i
   oldN       = head (curr !! i)
-  newN       = op oldN -- `div` 3
+  newN       = op oldN `mod` modVal
   nextIndexN = test newN
   newTot     = totInspections !!= (i, totInspections !! i + 1)
   newCurr =
@@ -45,23 +48,23 @@ doActions (curr, totInspections, ops, tests) i
       !!= (nextIndexN, (curr !! nextIndexN) ++ [newN])
 
 doRound
-  :: ([[Integer]], [Int], [Integer -> Integer], [Integer -> Int])
-  -> ([[Integer]], [Int], [Integer -> Integer], [Integer -> Int])
-doRound (c, i, o, t) = foldl doActions (c, i, o, t) [0 .. length c - 1]
+  :: ([[Int]], [Int], [Int -> Int], [Int -> Int], Int)
+  -> ([[Int]], [Int], [Int -> Int], [Int -> Int], Int)
+doRound (c, i, o, t, m) = foldl doActions (c, i, o, t, m) [0 .. length c - 1]
 
 main :: IO ()
 main = do
-  l <- splitOn "\n\n" <$> readFile "prob11/mini_input.txt"
+  l <- splitOn "\n\n" <$> readFile "prob11/input.txt"
   let starting = map parseStarting l
   let ops      = map parseOp l
   let test     = map parseTest l
-  let (curr, totInspections, _, _) = foldl
+  let modVal   = product $ map parseModVal l
+  print modVal
+  let (curr, totInspections, _, _, _) = foldl
         (\acc i -> doRound acc)
-        (starting, replicate (length starting) 0, ops, test)
+        (starting, replicate (length starting) 0, ops, test, modVal)
         [1 .. 10000]
   print curr
   print totInspections
   let sortedIs = sortOn Down totInspections
   print $ head sortedIs * sortedIs !! 1
-
--- 46316663904
